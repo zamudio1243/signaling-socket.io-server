124ebaf1f00a8c3bc9961b743da2d29838295c46
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2021-12-04 00:46:45 -0600

    :sparkles: Compilar codigo

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
 import { Configuration } from "@tsed/common";
 import "@tsed/platform-express";
 import "@tsed/socketio"; // import socket.io Ts.ED module
+import cors from "cors";
 import { Compiler } from "./services/Compiler";
+import bodyParser from "body-parser";
+
 @Configuration({
   rootDir: __dirname,
   socketIO: {},
   mount: {
     "/rest": [Compiler],
   },
+  middlewares: [
+    cors({
+      origin: "*"
+    }),
+    bodyParser.json(),
+    bodyParser.urlencoded()
+  ]
 })
 export class Server {}
